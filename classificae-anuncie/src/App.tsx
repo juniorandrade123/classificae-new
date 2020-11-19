1233ff162d85d14ba08b730a586881964d2df2c4
Geraldo Júnior <juninhuu.andrade@gmail.com>
2020-11-18 22:36:58 -0300

    anuncie

--- classificae-anuncie/src/App.tsx
+++ classificae-anuncie/src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row p-0 w-100">
+      <div className="w-20 sidebar-autentication">
+        <div className="row col-12">
+          <div className="col-12 text-center ml-auto mr-auto mt-4 logo">
+            <span>Classifica</span><span className="tage-header">e</span>
+          </div>
+          <div className="col-12 text-center ml-auto mr-auto mt-5 info">
+            <label>Faça login na sua conta</label>
+          </div>
+          <div className="col-12 ml-auto mr-auto mt-5">
+            <form>
+                <div className="row col-12 m-auto">
+                  <div className="col-12 ml-auto mr-auto p-0">
+                    <label className="name-label">Endereço de e-mail</label>
+                    <input type="text" className="form-control form-classificae"/>
+                  </div>
+                  <div className="col-12 ml-auto mr-auto p-0 mt-3">
+                    <label className="name-label">Senha de acesso</label>
+                    <input type="password" className="form-control form-classificae"/>
+                  </div>
+                  <div className="col-12 p-0 mt-3 ml-auto mr-auto">
+                    <button type="submit" className="form-control btn-classificae-primary">Acessar</button>
+                  </div>
+                  <div className="col-12 p-0 mt-5 text-center">                    
+                    <label className="register-me">
+                      Anunciar
+                    </label>
+                  </div>
+                </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ classificae-anuncie/src/page/register/register.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './register.scss';
+
+const Register: React.FC = () => {
+  return (
+      <div>
+
+      </div>
+  )
+}
+
+export default Register;
