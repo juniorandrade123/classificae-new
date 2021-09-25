6e6f0f8c1531ab9448f98841c7cab6f9094b4e42
Junior Andrade <junior.andrade@triasoftware.com.br>
2021-09-24 22:06:57 -0300

    ajust env

--- classificae-portal/src/api.tsx
+++ classificae-portal/src/api.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: 'http://classificae-api.herokuapp.com/api/v1/',    
+    baseURL: 'https://classificae-api.herokuapp.com/api/v1/',    
 });
 
 export default api;
